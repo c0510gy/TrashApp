47c98535a952c2ed5c4c6834f21274d201ef15d5
Sang-geon Yun <45253879+c0510gy@users.noreply.github.com>
2022-03-16 01:54:53 +0900

    feat: show simple list of trash cans

--- trash_app_project/lib/trashcanlist.dart
+++ trash_app_project/lib/trashcanlist.dart
@@ -1,28 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:csv/csv.dart';
 
+class TrashCanListPage extends StatefulWidget {
+  @override
+  _TrashCanListPage createState() => _TrashCanListPage();
+}
 
-class TrashCanListPage extends StatelessWidget {
+class _TrashCanListPage extends State<TrashCanListPage> {
 
-  final _listTextStyle = TextStyle(color: Colors.black, fontFamily: 'GyeonggiLight');
-  final mocklist = [
-    'hello, world',
-    'trash can 1',
-  ];
+  List<List<dynamic>> _data = [];
+
+  void _loadCSV() async {
+    final _rawData = await rootBundle.loadString("assets/trashcanlist.csv");
+    List<List<dynamic>> _listData = const CsvToListConverter().convert(_rawData);
+    setState(() {
+      _data = _listData;
+    });
+  }
+
+  _TrashCanListPage(){
+    _loadCSV();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-        padding: const EdgeInsets.all(8),
-        itemCount: mocklist.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Container(
-            child: Text(mocklist[index], style: _listTextStyle),
+    return ListView.builder(
+        itemCount: _data.length,
+        itemBuilder: (_, index) {
+          return Card(
+            margin: const EdgeInsets.all(3),
+            color: index == 0 ? Colors.amber : Colors.white,
+            child: ListTile(
+              leading: Text(_data[index][0].toString()),
+              title: Text(_data[index][1]),
+              subtitle: Text(_data[index][2].toString().replaceAll('\n', ' ')),
+              trailing: Text(_data[index][3].toString().replaceAll('\n', ' ')),
+            ),
           );
         },
-        separatorBuilder: (BuildContext context, int index) {
-          return Divider();
-        },
-    );
-
+      );
   }
 }
