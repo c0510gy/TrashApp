4e601b39c3f5765ca9145296b051afbf97152647
Sang-geon Yun <45253879+c0510gy@users.noreply.github.com>
2022-03-16 02:28:12 +0900

    feat: implement simple search

--- trash_app_project/lib/trashcanlist.dart
+++ trash_app_project/lib/trashcanlist.dart
@@ -8,37 +8,63 @@ class TrashCanListPage extends StatefulWidget {
 }
 
 class _TrashCanListPage extends State<TrashCanListPage> {
-
-  List<List<dynamic>> _data = [];
+  List<List<dynamic>> _trashCans = [];
+  List<List<dynamic>> _trashCansOnList = [];
 
   void _loadCSV() async {
     final _rawData = await rootBundle.loadString("assets/trashcanlist.csv");
-    List<List<dynamic>> _listData = const CsvToListConverter().convert(_rawData);
+    _trashCans = const CsvToListConverter().convert(_rawData);
     setState(() {
-      _data = _listData;
+      _trashCansOnList = _trashCans;
     });
   }
 
-  _TrashCanListPage(){
+  _TrashCanListPage() {
     _loadCSV();
   }
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: _data.length,
+    return Column(children: <Widget>[
+      TextField(
+        keyboardType: TextInputType.text,
+        onChanged: (text) {
+          List<List<dynamic>> newData = [
+            _trashCans[0],
+            ..._trashCans
+                .sublist(1)
+                .where((row) =>
+                    '${row[1]}${row[2]}${row[3]}'.toString().contains(text))
+                .toList()
+          ];
+          setState(() {
+            _trashCansOnList = newData;
+          });
+        },
+        decoration: InputDecoration(
+            hintText: '검색',
+            border: InputBorder.none,
+            icon: Padding(
+                padding: EdgeInsets.only(left: 13), child: Icon(Icons.search))),
+      ),
+      Expanded(
+          child: ListView.builder(
+        itemCount: _trashCansOnList.length,
         itemBuilder: (_, index) {
           return Card(
             margin: const EdgeInsets.all(3),
-            color: index == 0 ? Colors.amber : Colors.white,
+            color: index == 0 ? Colors.lightBlueAccent : Colors.white,
             child: ListTile(
-              leading: Text(_data[index][0].toString()),
-              title: Text(_data[index][1]),
-              subtitle: Text(_data[index][2].toString().replaceAll('\n', ' ')),
-              trailing: Text(_data[index][3].toString().replaceAll('\n', ' ')),
+              leading: Text(_trashCansOnList[index][0].toString()),
+              title: Text(_trashCansOnList[index][1]),
+              subtitle: Text(
+                  _trashCansOnList[index][2].toString().replaceAll('\n', ' ')),
+              trailing: Text(
+                  _trashCansOnList[index][3].toString().replaceAll('\n', ' ')),
             ),
           );
         },
-      );
+      )),
+    ]);
   }
 }
